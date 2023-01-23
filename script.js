7058c046d4271ea36f43c91d788c8425cf6325b9
Jeevana Mantina <113972063+Jeevanamsk@users.noreply.github.com>
2023-01-23 22:51:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your code here
+
+    var textbox = document.getElementById("evaluatedText");
+    var wordCount = document.getElementById("wordCount");
+    textbox.addEventListener("input", function() {
+        wordCount.innerHTML = textbox.value.trim().split(/\s+/).length + " words";
+    });
+
